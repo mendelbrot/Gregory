4a7201d5c7c3dc47fc9427332cb785a6032fd762
Gregory Maxedon <gregory.maxedon@gmail.com>
2018-12-11 10:37:39 -0800

    added a hello string

--- Gregory/Startup.cs
+++ Gregory/Startup.cs
@@ -46,6 +46,8 @@ namespace Gregory
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            string newChange = "hello";
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
