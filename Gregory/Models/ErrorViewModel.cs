49bc01acb2a7c6872fbb17da04054d3fccafadc5
Gregory Maxedon <gregory.maxedon@gmail.com>
2018-12-11 09:46:54 -0800

    Add project files.

---
+++ Gregory/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gregory.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
